88b17d9f61dbded1ee5494567f53b91b98526df4
tanqam <tanqam@gmail.com>
2026-06-06 15:57:25 +0530

    Clean Architecture code updated

---
+++ CleanArchitectureDemo/Controllers/ProductsController.cs
@@ -0,0 +1,41 @@
+﻿using Application.DTOs;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CleanArchitectureDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAll() => Ok(await _productService.GetAllAsync());
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id) => Ok(await _productService.GetByIdAsync(id));
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductDto dto)
+        {
+            await _productService.AddAsync(dto);
+            return Ok();
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateProductDto dto)
+        {
+            await _productService.UpdateAsync(dto);
+            return Ok();
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _productService.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
--- CleanArchitectureDemo/Program.cs
+++ CleanArchitectureDemo/Program.cs
@@ -1,6 +1,21 @@
+using Application.Interfaces;
+using Application.Mappings;
+using Application.Services;
+using Domain.Interfaces;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("connectionString")));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();  
+builder.Services.AddAutoMapper(typeof(MappingProfile).Assembly);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
